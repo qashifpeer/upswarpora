d2e1bf644e557c65a47cc7c224d669415d4e42fb
qashifpeer <mail.qashifpeer@gmail.com>
2022-09-19 09:10:50 +0530

    modified svg

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,13 +6,13 @@
 
             <div class="flex flex-col items-center justify-center pt-10">
                 <div class="flex flex-col relative mb-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <svg xmlns="http://www.w3.org/2000/svg" xlink:href="data:image/png;base64 viewBox="0 0 1440 320">
                         <path fill="#5000ca" fill-opacity="1"
                             d="M0,64L60,96C120,128,240,192,360,213.3C480,235,600,213,720,197.3C840,181,960,171,1080,186.7C1200,203,1320,245,1380,266.7L1440,288L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
                         </path>
                     </svg>
                     <img src="{{ asset('img/me.jpg') }}" alt=""
-                        class="w-24 h-24 absolute rounded-full top-10 shadow-xl left-20">
+                        class="w-24 h-24 absolute rounded-full top-24 shadow-xl left-20">
                 </div>
                 <h1 class="font-medium pt-3">Qashif Peer</h1>
                 <h2 class="text-sm font-thin">Teacher | Web Developer</h2>

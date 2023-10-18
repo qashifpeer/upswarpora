0f0d41dc2f488de88a5308d15db1e8187f07b0a5
qashifpeer <mail.qashifpeer@gmail.com>
2023-10-18 22:28:11 +0530

    initial code

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center bg-slate-950'>
-      <h2 className='text-2xl text-white'>welcome ups warpora</h2>
-      <h1 className='text-2xl text-white'>This site is under construction</h1>
+    <div className='flex flex-col justify-center items-center bg-slate-950 py-4 w-full h-screen'>
+      <h2 className='text-2xl text-white'>Welcome UPS Warpora</h2>
+      <h1 className='text-2xl text-white font-extrabold'>This site is under construction</h1>
     </div>
   )
 }
